34184970abe49cefd83d4ab44819c0b32c4f27d6
maria magdalena <neumariamagdalena@gmail.com>
2024-07-19 16:43:54 +0200

    add item model

---
+++ models/Item.model.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the Book model to whatever makes sense in this case
+const itemSchema = new Schema(
+  {
+    itemname: {
+      type: String,
+      required: [true, "itemname is required."],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "description is required."],
+    },
+    category: {
+      type: String,
+      enum: [
+        "electronics",
+        "beauty",
+        "music",
+        "tools",
+        "clothes",
+        "rooms",
+        "outdoor area",
+      ],
+      required: [true, "category is required."],
+    },
+
+    location: {
+      type: "String",
+      //maybe enum to have only a selected area to be able to choose from?
+    },
+    owner: {
+      type: Number,
+    },
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const Item = model("Item", itemSchema);
+
+module.exports = Item;
