82be6ffa912987d826fdd11d5f6956c4b40c5fbe
maria magdalena <neumariamagdalena@gmail.com>
2024-07-20 13:59:20 +0200

    wip edit borrowrequest

--- routes/borrowRequest.routes.js
+++ routes/borrowRequest.routes.js
@@ -25,4 +25,30 @@ router.get("/", async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 });
+
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedRequest = await BorrowRequest.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true, // Return the updated document
+        runValidators: true, // Run schema validations
+      }
+    )
+      .populate("owner")
+      .populate("borrower")
+      .populate("item");
+
+    if (!updatedRequest) {
+      return res
+        .status(404)
+        .json({ message: "No borrow request with this ID" });
+    }
+
+    res.json(updatedRequest);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 module.exports = router;
