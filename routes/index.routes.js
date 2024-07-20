7617bdd310da050b15e225df4dd7b637e6e92bcf
maria magdalena <neumariamagdalena@gmail.com>
2024-07-20 12:24:28 +0200

    items route working, but empty array

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const userRoutes = require("./user.routes");
+const itemRoutes = require("./item.routes");
 router.get("/", (req, res) => {
   res.json("Hello from the /api route!");
 });
 
 router.use("/users", userRoutes);
+router.use("/items", itemRoutes);
 
 module.exports = router;
