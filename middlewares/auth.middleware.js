74c2ce53f8c6a4af155e5941ca7e6d60c6484eb6
Hanna Komornitzyk <hanna.komornitzyk@gmail.com>
2024-07-29 10:53:56 +0200

    Merging Middleware and Auth

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -5,16 +5,13 @@ const isAuthenticated = (req, res, next) => {
   if (!authHeader) {
     return res.status(401).json({ message: "No token provided" });
   }
-
   const token = authHeader.split(" ")[1];
   jwt.verify(token, secret, (err, decoded) => {
     if (err) {
       return res.status(401).json({ message: "Invalid token" });
     }
-
     req.tokenPayload = decoded; // Attach the decoded user info to the request object
     next();
   });
 };
-
 module.exports = { isAuthenticated };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,19 +5,15 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { isAuthenticated } = require("../middlewares/auth.middleware");
 const secret = require("../config/secretGenerator");
-
 // All routes start with /auth
-
 // Health Check Route
 router.get("/", (req, res) => {
   res.json("All good in auth");
 });
-
 // POST Signup
 router.post("/signup", async (req, res, next) => {
   const salt = bcrypt.genSaltSync(13);
   const passwordHash = bcrypt.hashSync(req.body.password, salt);
-
   try {
     const newUser = await User.create({ ...req.body, passwordHash });
     res.status(201).json(newUser);
@@ -30,11 +26,9 @@ router.post("/signup", async (req, res, next) => {
     }
   }
 });
-
 // POST Login
 router.post("/login", async (req, res, next) => {
   const { username, password } = req.body;
-
   try {
     const potentialUser = await User.findOne({ username });
     if (potentialUser) {
